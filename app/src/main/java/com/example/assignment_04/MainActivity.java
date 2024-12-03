97ad2f069c0156e8c055256834cab41a9659fd61
Asus <mehhussumon47@gmail.com>
2024-12-04 04:07:33 +0600

    Initial commit

--- app/src/main/java/com/example/assignment_04/MainActivity.java
+++ app/src/main/java/com/example/assignment_04/MainActivity.java
@@ -1,25 +1,81 @@
 package com.example.assignment_04;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    Button btn;
+    String[] items = {"Apple", "Banana", "Grape", "Mango", "Pineapple"};
+    Integer[] imgid = {R.drawable.apple, R.drawable.banana, R.drawable.grape, R.drawable.mango, R.drawable.pineapple};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn = findViewById(R.id.btn_exp);
+        listView = findViewById(R.id.simpleListView);
+
+        CustomAdapter adapter = new CustomAdapter();
+        listView.setAdapter(adapter);
+
+        btn.setOnClickListener(v -> {
+            int position = listView.getCheckedItemPosition();
+            if (position != -1) {
+                String selectedFruit = items[position];
+                Intent intent = new Intent(MainActivity.this, Expandable.class);
+                intent.putExtra("selectedFruit", selectedFruit);
+                startActivity(intent);
+            } else {
+                Toast.makeText(MainActivity.this, "Please select a fruit first!", Toast.LENGTH_SHORT).show();
+            }
         });
+
+        listView.setOnItemClickListener((parent, view, position, id) ->
+                Toast.makeText(MainActivity.this, "Clicked: " + items[position], Toast.LENGTH_SHORT).show()
+        );
+    }
+
+    private class CustomAdapter extends ArrayAdapter<String> {
+        public CustomAdapter() {
+            super(MainActivity.this, R.layout.list_item, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item, parent, false);
+                holder = new ViewHolder();
+                holder.textView = convertView.findViewById(R.id.textview);
+                holder.imageView = convertView.findViewById(R.id.imageView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.textView.setText(items[position]);
+            holder.imageView.setImageResource(imgid[position]);
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+            TextView textView;
+            ImageView imageView;
+        }
     }
 }
